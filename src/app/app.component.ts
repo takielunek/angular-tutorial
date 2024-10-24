3be5171f2e589bdfe5db32d89c65215c6ffacca2
Karolina Jesionek <karolina.anna.jesionek@gmail.com>
2024-10-25 00:17:10 +0200

    completed lesson: 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from './home/home.component'
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello Piotr!</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ["./app.component.css"],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes";
 }
