20d02c546b57dc3cdd40982848e9bc8120300221
Karolina Jesionek <karolina.anna.jesionek@gmail.com>
2024-10-25 00:45:29 +0200

    all lessons completed

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -60,15 +60,16 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => (this.housingLocation = housingLocation));
   }
 
   submitApplication() {
     this.housingService.submitApplication(
-      this.applyForm.value.firstName ?? '',
-      this.applyForm.value.lastName ?? '',
-      this.applyForm.value.email ?? ''
+      this.applyForm.value.firstName ?? "",
+      this.applyForm.value.lastName ?? "",
+      this.applyForm.value.email ?? ""
     );
   }
 }
